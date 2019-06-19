5973ba182649f93f164d8dbdf846b7cec268885c
Caio Augusto <caioaugustov@gmail.com>
2019-06-19 00:30:02 -0300

    app

--- src/compoments/Login/index.js
+++ src/compoments/Login/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Background from '../../assets/girl.jpg'
 import styled from 'styled-components'
+import { Dimensions, StatusBar } from 'react-native'
+import { Transition } from 'react-navigation-fluid-transitions'
 
-const Title = styled.Text``;
+const widthScreen = Dimensions.get('window').width;
+const heightScreen = Dimensions.get('window').height;
+
+const BackgroundImg = styled.ImageBackground`
+  flex: 1;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const Logo = styled.Image`
+  width: ${widthScreen / 2};
+	height: ${heightScreen / 6};
+	background: #000;
+`;
 
 export default function index() {
-	return <Title>Login</Title>
+	return (
+		<>
+			<StatusBar hidden/>
+      <BackgroundImg source={Background} resizeMode='cover'>
+        <Transition shared="logo">
+          <Logo resizeMode='contain' source={require('../../assets/logo-white.png')} />
+        </Transition>
+      </BackgroundImg>
+		</>
+	)
 }
